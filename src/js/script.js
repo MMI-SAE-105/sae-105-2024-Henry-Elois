051c0badf54a7f823fa82a3b542c2c75ee6fc2c6
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2024-12-12 20:06:25 +0100

    css programmation

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-const toggle = document.querySelector(".menu-btn");
-const nav = document.querySelector(".menu");
+const toggle = document.querySelector(".header__menu-icon");
+const nav = document.querySelector(".header__nav menu");
 const page = document.body;
 
 if (toggle && nav) {
